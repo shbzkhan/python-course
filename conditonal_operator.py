c60b288d9705de750906c7a370c40b20cce11b12
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2026-09-18 22:40:09 +0530

    push cnditinal operator

---
+++ conditonal_operator.py
@@ -0,0 +1,12 @@
+num = -10
+
+if num > 0:
+    print("The number is positive.")
+else:
+    print("The number is not positive.")
+
+
+# second example
+age = int(input("Enter your age: "))
+x = "You are an adult." if age >= 18 else "You are a minor."
+print(x)
